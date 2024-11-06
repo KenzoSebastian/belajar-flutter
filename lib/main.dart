e8e0bb5f228bee590a5ce362e03ad949dbaa8cdc
KenzoSebastian <kenzosebastian4@gmail.com>
2024-11-06 10:57:00 +0700

    initial new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,3 @@
-// import 'package:belajar_flutter/page_route/gallery_page.dart';
-// import 'package:belajar_flutter/page_route/home_page.dart';
-// import 'package:belajar_flutter/page_route/photo_page.dart';
-
-import 'package:belajar_flutter/model.dart';
-// import 'package:belajar_flutter/switch.dart';
-// import 'package:belajar_flutter/drawer.dart';
-// import 'package:belajar_flutter/navigation/pageSatu.dart';
-// import 'package:belajar_flutter/dismissible.dart';
-// import 'package:belajar_flutter/dialog.dart';
-// import 'package:belajar_flutter/grid_view.dart';
-// import 'package:belajar_flutter/text_field_Controller.dart';
-// import 'package:belajar_flutter/text_field.dart';
-// import 'package:belajar_flutter/tabBar_widget.dart';
-// import 'package:belajar_flutter/appBar_widget.dart';
-// import 'package:belajar_flutter/date_format.dart';
-// import 'package:belajar_flutter/mapping_list.dart';
-// import 'package:belajar_flutter/dynamic_app.dart';
-// import 'package:belajar_flutter/extract_widget.dart';
-// import 'package:belajar_flutter/image_widget.dart';
-// import 'package:belajar_flutter/list_tile.dart';
-// import 'package:belajar_flutter/list_view.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,14 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Belajar Flutter',
-      home: ModelFlutter(),
-      // initialRoute: HomePage.routeName,
-      // routes: {
-      //   HomePage.routeName: (context) => const HomePage(),
-      //   GalleryPage.routeName: (context) => const GalleryPage(),
-      //   PhotoPage.routeName: (context) => const PhotoPage(),
-      // },
+      title: 'Belajar Flutter Provider',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Belajar Provider'),
+        ),
+      )
     );
   }
 }
